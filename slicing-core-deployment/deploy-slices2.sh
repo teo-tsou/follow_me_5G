d46f4c8df98b2c920ce41c742cc3aadf832abcbf
root <ttsourdinis@gmail.com>
2022-09-17 14:54:33 +0000

    adding some kubevirt templates

--- slicing-core-deployment/deploy-slices2.sh
+++ slicing-core-deployment/deploy-slices2.sh
@@ -14,7 +14,7 @@ kubectl create -f oai-nrf-slice3.yaml
 sleep 3; 
 kubectl create -f oai-amf.yaml 
 sleep 3;
-kubectl create -f oai-smf-slice1.yaml 
+kubectl create -f oai-smf-slice1.yaml
 sleep 3;
 kubectl create -f oai-smf-slice2.yaml 
 sleep 3;
@@ -24,8 +24,8 @@ kubectl create -f oai-spgwu-slice1.yaml
 sleep 3; 
 kubectl create -f oai-spgwu-slice2.yaml 
 sleep 3;
-kubectl create -f oai-ext-dn.yaml 
-sleep 3;
+#kubectl create -f oai-ext-dn.yaml 
+#sleep 3;
 kubectl create -f ueransim.yaml 
 sleep 3;
 kubectl create -f oai-gnbsim2.yaml
