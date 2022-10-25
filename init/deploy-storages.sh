45e05d2cd35ebf3feae9f53d04b96c5f883a4caf
Theodoros Tsourdinis <56459281+teo-tsou@users.noreply.github.com>
2022-10-25 14:37:45 +0200

    Update deploy-storages.sh

--- init/deploy-storages.sh
+++ init/deploy-storages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Install Local-Path & NFS Provisioner
+#Install NFS Provisioner
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/ && helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=k8s-master1 --set nfs.path=/var/lib/kubelet/migration/
 #Install Multus CNI
 git clone https://github.com/intel/multus-cni.git && cd multus-cni
