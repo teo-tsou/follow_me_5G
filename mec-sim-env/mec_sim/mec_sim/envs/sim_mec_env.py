365fff283f913fbd390f5f50de3a6e77e182e8d5
Theodoros Tsourdinis <56459281+teo-tsou@users.noreply.github.com>
2023-02-06 20:01:48 +0100

    Adding license

--- mec-sim-env/mec_sim/mec_sim/envs/sim_mec_env.py
+++ mec-sim-env/mec_sim/mec_sim/envs/sim_mec_env.py
@@ -1,3 +1,21 @@
+# GNU General Public License version 3 (GPL-3.0)
+
+# Copyright (C) [2022-2023] [Theodoros Tshourdinis - Sorbonne University/UTH, email: theodoros.tsourdinis@sorbonne-universite.fr]
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import gym
 from gym import spaces
 import numpy as np
